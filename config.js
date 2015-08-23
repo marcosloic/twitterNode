b5cb771068341f8ac3fc084a0df8573880380263
coriolan <marcosloic@gmail.com>
2015-08-24 00:06:39 +0200

    Updated Twitter keys
    Updated Twitter keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'g9xpOwRQvn1Yy5c2cOip28V2B',
-    consumer_secret: 'hTP1CpviMHEJcdXpWUC8s44qHjOCAuqkpcZBKm3qtGb7Uo2GTi',
-    access_token_key: '1122906733-oiqpR1vUWWxY5EMNCUzL4Jmnlfnq72400sBcm7k',
-    access_token_secret: 'DK0Ql7QFlzFfqOhnEOgrZeQSXXMFGtLbKfQyUJdC2BVmG'
+    consumer_key: 'JLH96wRsb7y18gExbN7iwMifk',
+    consumer_secret: 'vMpnkNnxDBzKM1spDPBcA81s8MyRi6PaGcRzKwQjr3TswSSAXm',
+    access_token_key: '522072625-XZJhSVZTlPFUT1P8VeNFl5EGssNbcQUguSccQ0On',
+    access_token_secret: 'utbPqkvClY7bbwB4DVXcV0CWrDd7ASFPCvcBgwTTRBcL0'
   }
 }
