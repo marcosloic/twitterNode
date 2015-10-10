bf360fe60fc022307759b81f01154a4515deeb5d
Loïc Marcos <marcosloic@gmail.com>
2015-10-10 17:57:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express'),
 
 
 var app = express();
-var port = process.env.PORT || 8888;
+var port = process.env.PORT || 80;
 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
